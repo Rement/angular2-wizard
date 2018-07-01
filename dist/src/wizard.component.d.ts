1733e4901c4bf18e6b661322fca5cb4d92413d65
Andrei Kaliachka <akaliachka@gmail.com>
2018-07-01 14:30:34 +0300

    DIST adding

---
+++ dist/src/wizard.component.d.ts
@@ -0,0 +1,21 @@
+import { EventEmitter, QueryList, AfterContentInit, OnInit } from '@angular/core';
+import { WizardStepComponent } from './wizard-step.component';
+export declare class WizardComponent implements AfterContentInit, OnInit {
+    wizardSteps: QueryList<WizardStepComponent>;
+    private _steps;
+    private _isCompleted;
+    onStepChanged: EventEmitter<WizardStepComponent>;
+    constructor();
+    ngOnInit(): void;
+    ngAfterContentInit(): void;
+    readonly steps: Array<WizardStepComponent>;
+    readonly isCompleted: boolean;
+    activeStep: WizardStepComponent;
+    readonly activeStepIndex: number;
+    readonly hasNextStep: boolean;
+    readonly hasPrevStep: boolean;
+    goToStep(step: WizardStepComponent): void;
+    next(): void;
+    previous(): void;
+    complete(): void;
+}
